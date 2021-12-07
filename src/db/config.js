f6ac7fe830235569f5dfa57d835ec713d978c443
Thiago Aguiar <thiagoabaguiar@gmail.com>
2021-12-07 11:44:52 -0300

    update files 07-12

--- src/db/config.js
+++ src/db/config.js
@@ -1,11 +1,5 @@
-const sqlite3 = require("sqlite3")
-const { open } = require("sqlite")
+const sqlite3 = require("sqlite3") // serão usadas todas as funcionalidades
+const { open } = require("sqlite") // será usada apenas a funcionalidade "open"
 
 
-module.exports = () => {
-    open({
-        filename: "./database.sqlite",
-        driver: sqlite3.Database
-    })
-};
-
+module.exports = () => open({ filename: "./database.sqlite", driver: sqlite3.Database })
---
+++ src/db/init.js
@@ -0,0 +1,91 @@
+const Database = require("./config")
+
+const initDb = {
+
+    async init() {
+
+        const db = await Database() // para abrir o banco
+
+        await db.exec(
+            `CREATE TABLE profile (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                avatar TEXT,
+                price_per_hour INT,
+                monthly_budget INT,
+                days_per_week INT,
+                hours_per_day INT,
+                vacation_per_year INT
+            )`
+        );
+
+        await db.exec(
+            `CREATE TABLE jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                daily_hours INT,
+                total_hours INT,
+                budget INT,
+                created_at DATETIME
+            )`
+        );
+
+        await db.run(
+            `INSERT INTO profile (
+                name,
+                avatar,
+                price_per_hour,
+                monthly_budget,
+                days_per_week,
+                hours_per_day,
+                vacation_per_year
+            ) VALUES (
+                "Thiago Aguiar",
+                "https://github.com/thiagoabaguiar.png",
+                75,
+                5000,
+                5,
+                8,
+                4
+            )`
+        );
+
+        await db.run(
+            `INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                budget,
+                created_at
+            ) VALUES (
+                "Pizzaria Guloso",
+                10,
+                10,
+                5000,
+                1638478485
+            )`
+        );
+
+        await db.run(
+            `INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                budget,
+                created_at
+            ) VALUES (
+                "OneTwo Project",
+                4,
+                20,
+                4000,
+                1638458285
+            )`
+        );
+
+        await db.close() // para fechar o banco
+    }
+}
+
+//////////////////////////////////////////////////////////////////
+
+initDb.init()
